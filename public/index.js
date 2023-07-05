6454fb1916e89e60338ae3c75cf596ff39b53512
Tanya <tanya.nijhawan2021@vitstudent.ac.in>
2023-07-05 16:29:09 +0530

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,56 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('note-form');
+    const titleInput = document.getElementById('title-input');
+    const contentInput = document.getElementById('content-input');
+    const notesList = document.getElementById('notes-list');
+  
+    // Fetch all notes from the server
+    fetch('http://localhost:3500/api/notes')
+      .then(response => response.json())
+      .then(notes => {
+        notes.forEach(note => {
+          displayNoteInList(note);
+        });
+      })
+      .catch(error => console.error('Error:', error));
+  
+    // Add new note on form submit
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+  
+      const title = titleInput.value;
+      const content = contentInput.value;
+  
+      if (title.trim() === '' || content.trim() === '') {
+        return;
+      }
+  
+      const newNote = { title, content };
+  
+      fetch('http://localhost:3500/api/notes', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newNote),
+      })
+        .then(response => response.json())
+        .then(note => {
+          displayNoteInList(note);
+          titleInput.value = '';
+          contentInput.value = '';
+        })
+        .catch(error => console.error('Error:', error));
+    });
+  
+    // Display a note in the list
+    function displayNoteInList(note) {
+      const noteElement = document.createElement('div');
+      noteElement.classList.add('note');
+      noteElement.innerHTML = `
+        <h3>${note.title}</h3>
+        <p>${note.content}</p>
+      `;
+      notesList.appendChild(noteElement);
+    }
+  });
+  
---
+++ server/server.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import connectDB from './mongodb/db.js';
+import cors from 'cors';
+import Note from './mongodb/models/notemodel.js';
+import path from 'path';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+// Middleware
+app.use(express.json());
+
+const router = express.Router();
+
+// Serve static files from the frontend directory
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'public')));
+
+// GET request for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// GET all notes
+router.get('/api/notes', async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.json(notes); // Make sure to return the notes array as the response
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+// POST a new note
+router.post('/api/notes', async (req, res) => {
+  const note = new Note({
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  try {
+    const newNote = await note.save();
+    res.status(201).json(newNote);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Register the router middleware
+app.use(router);
+
+const server = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(3500, () => console.log('Server is running on port 3500'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+server();
+
